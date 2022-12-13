75bf13ff2927d1dc0e20e9b24e56597f57f9268e
Richard Lovell <richjavalabs@gmail.com>
2022-12-13 23:00:54 +1300

    first commit

---
+++ theme/constants.js
@@ -0,0 +1,25 @@
+export const pages = {
+  HOME: "home",
+  SERVICES: "services",
+  BLOG: "blog",
+  BLOG_ARTICLE: "blog-article",
+  AUTHOR_ARTICLE: "author-article",
+  CONTACT: "contact",
+  ABOUT: "about",
+  COVERS: "covers",
+  BANNERS: "banners",
+  BLOCKS: "blocks",
+  CARDS: "cards",
+  FORMS: "forms",
+  LISTS: "lists",
+};
+
+export const pageTypes = {
+  SITE: "site",
+  TEMPLATES: "templates",
+};
+
+export const contentTypes = {
+  BLOG_ITEM: "blog-item",
+  SERVICE: "service",
+};
